aa5cbdc965500695a36bf5ab47d8863c29888ec8
kumarbaberwal <itsnitesh16@gmail.com>
2025-08-22 23:49:50 +0530

    inngest setup, user model created

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./config/inngest.js"
 
 const app = express();
 app.use(express.json());
 app.use(clerkMiddleware());
-
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 app.get("/", (req, res) => {
@@ -17,7 +20,20 @@ app.get("/", (req, res) => {
 
 const PORT = ENV.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    connectDB();
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (ENV.NODE_ENV !== "production") {
+            app.listen(PORT, () => {
+                console.log(`Server is running on http://localhost:${PORT}`);
+            })
+        }
+    } catch (error) {
+        console.log("Error starting server: ", error);
+        process.exit(1);
+    }
+}
+
+startServer()
+
+export default app;
